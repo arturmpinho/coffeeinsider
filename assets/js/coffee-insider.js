d507d9771cf5b3399d6fc8051e320d15c03209b2
arturmpinho <arturpinho@gmail.com>
2020-12-14 21:11:29 +0000

    Added select boxes in calc.html and populated them with coffees.json file via jQuery

--- assets/js/coffee-insider.js
+++ assets/js/coffee-insider.js
@@ -17,5 +17,20 @@ $(document).ready(function() {
         $("#main-btn-lock").show();
         $(".sub-btns").hide();
     })
-
 })
+
+/* Coffees Select List */
+
+let dropdown = $('.dropdownCoffee');
+
+dropdown.empty();
+dropdown.append('<option selected="true">Select your Coffee</option>');
+dropdown.prop('selectedIndex', 0);
+
+const url = 'assets/js/coffees.json';
+
+$.getJSON(url, function (data) {
+  $.each(data, function (key, entry) {
+    dropdown.append($('<option></option>').text(`${entry.country} - ${entry.coffees}`));
+  })
+});
