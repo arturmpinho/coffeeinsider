93a2b91b584dab2e442e0dd7cb0ee0b622a507d0
arturmpinho <arturpinho@gmail.com>
2021-01-04 12:47:54 +0000

    Created functionality to fetch coffee related articles from The NY Times, display them in cards and proper styling

---
+++ assets/js/nyTimes.js
@@ -0,0 +1,26 @@
+const apiKey = "Jxt0w4CNFEgH6AGwj6Rdc6VhMU6UGphm"
+
+let url = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=coffee&api-key=${apiKey}`;
+
+$.ajax({
+    url: url,
+    method: "GET",
+}).done(function(result){
+
+    for (let i = 0; i<result.response.docs.length; i++) {
+        console.log(result.response.docs[i]);
+        let div = document.getElementById("news");
+        div.innerHTML += `  <div class="col-md-6 col-lg-4">
+                                <div class="card bg-transparent">
+                                    <img src="https://static01.nyt.com/${result.response.docs[i].multimedia[32].url}" class="card-img-top" alt="News thumbnail">
+                                    <div class="card-body">
+                                    <h2 class="card-title card-title-news mb-5">${result.response.docs[i].abstract}</h2>
+                                    <p class="card-text card-text-news mb-3">${result.response.docs[i].lead_paragraph}</p>
+                                    <p class="news-date">${result.response.docs[i].pub_date}</p>
+                                    <a href="${result.response.docs[i].web_url}" target="_blank" class="btn ci-btns mb-5">Check this article</a>
+                                    </div>
+                                </div>
+                            </div>`
+        }
+}).fail(function(err){
+});
